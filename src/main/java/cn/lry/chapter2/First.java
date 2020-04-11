159556ac64d220c363c4a3e012d1de82fd6fe5bd
bombasticRY <504073254@qq.com>
2020-04-11 23:38:29 +0800

    回答Chapter第二问 第三问问题

--- src/main/java/cn/lry/chapter2/First.java
+++ src/main/java/cn/lry/chapter2/First.java
@@ -1,7 +1,10 @@
 package cn.lry.chapter2;
 
+import java.text.SimpleDateFormat;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
+import javax.swing.*;
+import javax.swing.text.DateFormatter;
 
 public class First {
 
@@ -13,6 +16,22 @@ public class First {
         // 略复杂类型推断 指定类型为long
         BinaryOperator<Long> add = (x, y) -> x + y;
 
-        //ThreadLocal
+        ThreadLocal<DateFormatter> a = ThreadLocal.withInitial(()-> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+
+        //Runnable helloWorld = () -> System.out.println("hello world");
+
+      /*  JButton button = new JButton();
+        button.addActionListener(event ->
+            System.out.println(event.getActionCommand()));*/
+
+
     }
 }
+
+/*
+interface IntPred{
+    boolean test(Integer value);
+}
+    boolean check(Predicate<Integer> predicate);
+
+    boolean check(IntPred perdicate);*/
